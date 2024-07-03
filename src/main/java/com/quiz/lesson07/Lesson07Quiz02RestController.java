d0bf532c67cd3b380585583e7a903385c6760b74
DESKTOP-OQIQ52P\FORYOUCOM <jun062202@gmail.com>
2024-07-03 20:12:46 +0900

    2024-07-03 Home

--- src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
+++ src/main/java/com/quiz/lesson07/Lesson07Quiz02RestController.java
@@ -25,45 +25,46 @@ public class Lesson07Quiz02RestController {
 		
 		return recruitRepository.findById(8);
 	}
-	
+	// http://localhost:8080/lesson07/quiz02/2?companyId=1
 	@GetMapping("/2")
 	public List<RecruitEntity> quiz01_2(
-			@RequestParam("id") int id) {
-		// id가 8인 공고를 조회
+			@RequestParam("companyId") int companyId) {
+		// Request Parameter로 전달 받은 company id로 해당하는 회사의 공고를 조회하세요
 		
-		return recruitRepository.findById(id);
+		return recruitRepository.findByCompanyId(companyId);
 	}
 	
 	@GetMapping("/3")
-	public List<RecruitEntity> quiz01_2(
-			@RequestParam("id") int id) {
+	public List<RecruitEntity> quiz01_3() {
 		// id가 8인 공고를 조회
 		
-		return recruitRepository.findById(id);
+		return recruitRepository.findByPositionAndType("웹 back-end 개발자","정규직");
 	}
+	
 	
 	@GetMapping("/4")
-	public List<RecruitEntity> quiz01_2(
-			@RequestParam("id") int id) {
-		// id가 8인 공고를 조회
+	public List<RecruitEntity> quiz01_4() {
+		// 정규직이거나 연봉이 9000 이상인 공고를 조회하고 아래와 같이 출력하세요.
 		
-		return recruitRepository.findById(id);
+		return recruitRepository.findByTypeOrSalaryGreaterThan("정규직", 9000);
 	}
 	
+	
 	@GetMapping("/5")
-	public List<RecruitEntity> quiz01_2(
-			@RequestParam("id") int id) {
-		// id가 8인 공고를 조회
+	public List<RecruitEntity> quiz01_5() {
+		// 계약직 목록을 연봉 기준으로 내림차순 정렬해서 3개만 조회하세요.
 		
-		return recruitRepository.findById(id);
+		return recruitRepository.findTop3ByTypeOrderBySalaryDesc("계약직");
 	}
+	
 	@GetMapping("/6")
-	public List<RecruitEntity> quiz01_2(
-			@RequestParam("id") int id) {
-		// id가 8인 공고를 조회
+	public List<RecruitEntity> quiz01_6() {
+		// 성남시 분당구가 지역인 연봉 7000 이상 8500 이하인 공고를 조회하고 아래와 같이 출력하세요.
 		
-		return recruitRepository.findById(id);
+		return recruitRepository.findByRegionAndSalaryBetween("성남시 분당구", 7000, 8500);
 	}
+	
+	
 	
 	
 }
--- src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
+++ src/main/java/com/quiz/lesson07/repository/RecruitRepository.java
@@ -1,11 +1,17 @@
 package com.quiz.lesson07.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.quiz.lesson07.entity.RecruitEntity;
 
 public interface RecruitRepository extends JpaRepository<RecruitEntity, Integer>{
 
-	
+	public List<RecruitEntity> findByCompanyId(int companyId);
+	public List<RecruitEntity> findByPositionAndType(String position, String type);
+	public List<RecruitEntity> findByTypeOrSalaryGreaterThan(String type, int salary);
+	public List<RecruitEntity> findTop3ByTypeOrderBySalaryDesc(String type);
+	public List<RecruitEntity> findByRegionAndSalaryBetween(String string, int salaryStart, int salaryEnd);
 	
 }
